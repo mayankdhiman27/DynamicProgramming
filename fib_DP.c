84219227c94e21d6a1fe86665d140f3e901ae519
Dhiman Mayank <mayankdhiman27@gmail.com>
2017-10-14 07:54:00 +0530

    Update fib_DP.c

--- fib_DP.c
+++ fib_DP.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 
 void main(){
 int n;
@@ -6,12 +7,16 @@ scanf("%d",&n);
 int fib[n];
 fib[0]=0;
 fib[1]=1;
+long long int fib_ans=1;
 for(int i=2;i<n;i++){
 fib[i]=fib[i-1]+fib[i-2];
+fib_ans=fib[i]%(int)(pow(10.0,8.0)+7);
 }
 
-for(int j=0;j<n;j++){
+/*for(int j=0;j<n;j++){
 printf("%d ",fib[j]);
-}
+}*/
+
+printf("%llu",fib_ans);
 printf("\n");
 }
